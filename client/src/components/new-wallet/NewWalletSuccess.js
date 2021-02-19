4a0d4cd91ae36092c1b30db975403c66e9a6f54c
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-19 10:11:01 +0530

    new wallet success styling complete

--- client/src/components/new-wallet/NewWalletSuccess.js
+++ client/src/components/new-wallet/NewWalletSuccess.js
@@ -7,7 +7,7 @@ function NewWalletSuccess({ history }) {
         <div className="container py-5" align='center'>
 
             <div style={{ height: '70vh', width: '90vw' }} className='bg-success d-flex flex-column justify-content-center align-items-center'>
-                <i className="far fa-check-circle fa-10x"></i>
+                <i className="fas fa-check-circle fa-10x"></i>
                 <p className='my-5'> New Wallet Successfully Created!</p>
             </div>
 
